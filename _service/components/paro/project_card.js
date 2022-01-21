67a2d0de704610cdf181a506395a1836b58daf23
vencax <vencax@centrum.cz>
2022-01-22 00:00:14 +0100

    paro app basics

--- _service/components/paro/project_card.js
+++ _service/components/paro/project_card.js
@@ -2,14 +2,20 @@ export default {
   props: ['proj', 'call'],
   computed: {
     url: function () {
-      return `/paro/${this.call.id}/${this.proj.id}`
+      return {
+        name: 'paroproject_detail',
+        params: {
+          callid: this.call.id,
+          id: this.proj.id
+        }
+      }
     }
   },
   template: `
     <div class="column">
       <img :src="proj.photo" /><br/>
       <hgroup>
-        <h4><a :href="url">{{ proj.name }}</h4>
+        <h4><router-link :to="url">{{ proj.name }}</router-link>
         <h5>{{ proj.desc }}</h5>
       </hgroup>
     </div>
